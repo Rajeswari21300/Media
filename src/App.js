820c0a313119277e3362ab935bf5d4612a15437e
Rajeswari Suresh <106241069+Rajeswari21300@users.noreply.github.com>
2023-10-01 16:39:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Top from './components/Top';
+import Bottom from './components/Bottom';
+
+
+
+
+function App() {
+  return (
+   <>
+    <Top />
+    <Bottom />
+    </>
+
+    
+  );
+}
+
+export default App;
